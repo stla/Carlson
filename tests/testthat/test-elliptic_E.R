f94b2e918070f18887cbc5b3a398dfbc7faca29b
stla <laurent_step@yahoo.fr>
2020-02-03 23:01:59 +0100

    more tests

---
+++ tests/testthat/test-elliptic_E.R
@@ -0,0 +1,7 @@
+context("elliptic_E")
+
+test_that("phi = pi/2", {
+  expect_equal(elliptic_E(pi/2,-1), sqrt(2)*elliptic_E(pi/2,1/2))
+  # expect_equal(elliptic_E(pi/2, 0.5),
+  #              pi/2*as.complex(gsl::hyperg_2F1(-1/2,1/2,1,0.5))) bug gsl!!
+})
--- tests/testthat/test-elliptic_F.R
+++ tests/testthat/test-elliptic_F.R
@@ -4,4 +4,6 @@ test_that("phi = pi/2", {
   expect_equal(elliptic_F(pi/2,-1), as.complex(gamma(1/4)^2/4/sqrt(2*pi)))
   expect_equal(elliptic_F(pi/2,1/2),
                as.complex(8*pi^(3/2)/gamma(-1/4)^2))
+  expect_equal(elliptic_F(pi/2, 0.5),
+               pi/2*as.complex(gsl::hyperg_2F1(1/2,1/2,1,0.5)))
 })

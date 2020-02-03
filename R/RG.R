1c5a596d42caa702bafdae99bd4ca3320596924d
stla <laurent_step@yahoo.fr>
2020-02-03 19:27:55 +0100

    update Carlson_RG

--- R/RG.R
+++ R/RG.R
@@ -1,12 +1,15 @@
 #' Carlson elliptic integral RG
 #' @description Evaluate the Carlson elliptic integral RG.
 #'
-#' @param x,y,z real or complex numbers; at least...
-#' @param minerror bound of relative error
+#' @param x,y,z real or complex numbers; at most one can be zero
+#' @param minerror bound on the relative error
 #'
 #' @return A complex number.
 #' @export
 Carlson_RG <- function(x, y, z, minerror = 2*.Machine$double.eps){
+  if(sum(c(x,y,z)==0) > 1){
+    stop("At most one of `x`, `y`, `z` can be 0.")
+  }
   x <- as.complex(x); y <- as.complex(y); z <- as.complex(z)
   (z*Carlson_RF(x, y, z, minerror) -
      (x-z)*(y-z)*Carlson_RD(x, y, z, minerror)/3 +

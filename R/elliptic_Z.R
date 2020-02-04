730eb6bbc621f8d1b596bbbee8285cee366fca73
stla <laurent_step@yahoo.fr>
2020-02-04 12:02:31 +0100

    elliptic_Z for m=1

--- R/elliptic_Z.R
+++ R/elliptic_Z.R
@@ -9,8 +9,20 @@
 #' @return A complex number.
 #' @export
 elliptic_Z <- function(phi, m, minerror = 2*.Machine$double.eps){
-  if(m == 1 && abs(Re(phi)) <= pi/2){
-    sin(phi)
+  if(m == 1){
+    if(abs(Re(phi)) <= pi/2){
+      sin(phi)
+    }else if(Re(phi) > pi/2){
+      while(Re(phi) > pi/2){
+        phi <- phi - pi
+      }
+      sin(phi)
+    }else{
+      while(Re(phi) < pi/2){
+        phi <- phi + pi
+      }
+      sin(phi)
+    }
   }else{
     elliptic_E(phi, m, minerror) -
       elliptic_E(pi/2, m, minerror)/elliptic_F(pi/2, m, minerror) *

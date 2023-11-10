b7aa2b0cd3c1b32ac01e0de23d4fe2adef751761
stla <laurent_step@yahoo.fr>
2023-11-10 09:40:29 +0100

    test Rcpp complex NaN

---
+++ inst/essais/Rcpp_complexNAN.cpp
@@ -0,0 +1,19 @@
+#include <Rcpp.h>
+
+// [[Rcpp::export]]
+Rcpp::ComplexVector test() {
+  Rcomplex z;
+  z.r = NAN;
+  z.i = 0;
+  return Rcpp::ComplexVector::create(z);
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically
+// run after the compilation.
+//
+
+/*** R
+test()
+*/
